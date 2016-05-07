1c4d584b693491f372a383cbe80c130c3ca758c7
sparkh <sparkh42@gmail.com>
2016-05-07 19:23:06 +0200

    prepare comment_dish file

---
+++ controllers/comment_dish.js
@@ -0,0 +1,14 @@
+var mysql = require("mysql");
+var nJwt = require('njwt');
+var utils = require("../utils/utils.js");
+
+function REST_ROUTER(router,connection,md5, secretKey) {
+    var self = this;
+    self.handleRoutes(router,connection,md5, secretKey);
+}
+
+REST_ROUTER.prototype.handleRoutes= function(router,connection,md5, secretKey) {
+	
+}
+
+module.exports = REST_ROUTER;
